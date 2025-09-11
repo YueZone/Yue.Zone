91307e65b947828a45f053255e1372dc80d5d8f0
Yue-plus <Yue_plus@foxmail.com>
2025-09-11 10:39:19 +0800

    feat: 添加 Tailwind CSS 支持并配置相关依赖

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://docs.astro.build/zh-cn/reference/configuration-reference/
 export default defineConfig({
-    site: 'https://yue.zone',
-    base: '/',
+  site: 'https://yue.zone',
+  base: '/',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
